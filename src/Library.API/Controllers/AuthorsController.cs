40d21f75139f63dbbb7cdfd64cc4c5faefc8ce76
Anand Upadhyay <upadhyayap.smit@gmail.com>
2019-10-10 15:56:07 +0530

    Initial commit

---
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,45 @@
+﻿using Library.API.Entities;
+using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Library.API.Helpers;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authors")] // [Route("api/[controller]")] to include the underlying controller class name
+    public class AuthorsController : Controller
+    {
+        private readonly ILibraryRepository libraryRepository;
+
+        public AuthorsController(ILibraryRepository libraryRepository)
+        {
+            this.libraryRepository = libraryRepository;
+        }
+
+        [HttpGet()]
+        public IActionResult getAuthors()
+        {
+            var authors = libraryRepository.GetAuthors();
+            var taperedAuthors = new List<AuthorDto>();
+
+            foreach (var author in authors)
+            {
+                taperedAuthors.Add(new AuthorDto
+                {
+                    Id = author.Id,
+                    Name = $"{author.FirstName} {author.LastName}",
+                    Genere = author.Genre,
+                    Age = author.DateOfBirth.GetCurrentAge()
+                });  
+
+            }
+
+
+            return new JsonResult(taperedAuthors);
+        }
+    }
+}
